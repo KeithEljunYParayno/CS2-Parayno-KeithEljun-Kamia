b4c9b265cfb5e4b9ff35c3f78e1028db96daea2d
KeithEljunYParayno <keitheljun.parayno@smc.pshs.edu.ph>
2025-11-23 14:27:51 +0800

    Add files via upload

---
+++ FA5.py
@@ -0,0 +1,17 @@
+places = []
+
+print("Please enter your 5 travel destinations:")
+for i in range(5):
+    places.append(input(f"Destination {i+1}: "))
+
+print("\nOriginal Travel Itinerary:")
+for i in range(5):
+    print(f"{i+1}. {places[i]}")
+
+print("Let's update your 2nd and 5th destinations.")
+places[1] = input("Enter a new destination for position 2: ")
+places[4] = input("Enter a new destination for position 5: ")
+
+print("Updated Travel Itinerary:")
+for i in range(5):
+    print(f"{i+1}. {places[i]}")
