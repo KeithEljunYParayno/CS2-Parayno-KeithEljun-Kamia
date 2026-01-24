23df3d07c4e995cd8e7f7a3554e33c9b6574b517
KeithEljunYParayno <keitheljun.parayno@smc.pshs.edu.ph>
2026-01-24 16:32:30 +0800

    Add files via upload

---
+++ Parayno_Q3_FA06.py
@@ -0,0 +1,8 @@
+student = {}
+student["name"] = input("Enter your name: ")
+student["age"] = int(input("Enter your age: "))
+student["subject"] = input("Enter your favorite subject: ")
+print("Student Record:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Favorite Subject:", student["subject"])
